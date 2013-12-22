d07c7b812beef73dfea4ce49468ee4eaecf89d8d
Andrew Volozhanin <linuxheadrus@gmail.com>
2013-12-23 02:37:53 +0600

    Change default port to 4000 to prevent clashes with rails

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
